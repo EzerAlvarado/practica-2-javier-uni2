a4ee17804d75ab1a6955cebaa9e1041706f17dfe
Ezer Jehu Alvarado <ezer.alvarado@sigob.com.mx>
2024-10-17 19:02:54 -0700

    practica de react

---
+++ src/pages/users/users.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importar Bootstrap
+
+const Users = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">Users Page</h1>
+      <div className="text-center">
+        <button className="btn btn-primary">Go to Dashboard</button>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
